1d51e11f7d50712ad1e59995eeb896448a9a8338
hanni66 <haeunkim0807@naver.com>
2022-07-06 18:28:49 +0900

    Feat : 내 위치로 이동하기 버튼 생성

--- MyMap/MyMap/ViewController.swift
+++ MyMap/MyMap/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import MapKit   // 지도를 사용하기 위해 import를 해준다.
+import MapKit           // 지도를 사용하기 위해 import를 해준다.
 import CoreLocation     // GPS를 사용하기 위해 import를 해준다.
 
 class ViewController: UIViewController {
@@ -24,10 +24,20 @@ class ViewController: UIViewController {
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.startUpdatingLocation()     // startUpdate를 해야 didUpdateLocation 메서드가 호출된다.
         manager.delegate = self
-        
         return manager
     }()
     
+    // 내 위치로 돌아가는 버튼을 생성한다.
+    lazy var locationBtn: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("내 위치", for: .normal)
+        btn.backgroundColor = .systemGray
+        btn.setTitleColor(.white, for: .normal)
+        btn.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        btn.tag = 1
+        return btn
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,6 +46,8 @@ class ViewController: UIViewController {
         
         // addSubview를 해주고 아래 제약조건을 작성해야 됨.
         self.view.addSubview(mapView)
+        // btn을 넣어준다.
+        self.view.addSubview(locationBtn)
         
         // View 위치 설정(제약조건)
         mapView.translatesAutoresizingMaskIntoConstraints = false
@@ -44,6 +56,23 @@ class ViewController: UIViewController {
         mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         
+        // btn 위치 설정(제약 조건)
+        locationBtn.translatesAutoresizingMaskIntoConstraints = false
+        locationBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        locationBtn.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        locationBtn.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+        locationBtn.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        print("내 위치로 이동")
+        let btnsendtag: UIButton = sender
+        if btnsendtag.tag == 1 {
+            DispatchQueue.main.async {
+                self.mapView.setUserTrackingMode(.follow, animated: true)   // 위치에 따라 화면이 바뀐다.
+            }
+            dismiss(animated: true, completion: nil)
+        }
     }
     
     func getLocationUsagePermission() {
